dc535f04740557c1caf258d1bc9025caf21ef5d4
Tanisha <tanishamajumder23@gmail.com>
2026-03-20 21:48:03 +0530

    first time implementation of haar cascade

--- webcam.py
+++ webcam.py
@@ -1,6 +1,10 @@
 import cv2 #Imports OpenCV library
 cap=cv2.VideoCapture(0) # opens the webcamera and 0 is our default laptop camera here
 # cap = object that controls your camera
+
+# Load Haar Cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
 while True: # infinite loop because video= continuous frames
     ret, frame=cap.read() # frame = the NumPy array image and the ret = whether the frame was captured sucessfully
     #ret = did it work?  &  frame = the image
@@ -8,8 +12,16 @@ while True: # infinite loop because video= continuous frames
         break
      # Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw rectangles
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imshow("Webcam", frame)
     # Show grayscale frame
-    cv2.imshow("Webcam", gray)
+    #cv2.imshow("Webcam", gray)
     #We convert to grayscale because face detection focuses on facial 
     #structures (like edges and shapes), not colors. 
     #Grayscale reduces data, making processing faster, 
